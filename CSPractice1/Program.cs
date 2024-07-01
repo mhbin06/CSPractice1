3ec6332820834e562bdd5435b86c17b185a58767
munhyebin <munhyebin40@gmail.com>
2024-07-01 13:57:07 +0900

    #5 22-7 소멸자

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -18,6 +18,10 @@ namespace CSPractice1
         {
             Console.WriteLine("(인스턴스) 생성자 호출");
         }
+        ~Sample()
+        {
+            Console.WriteLine("소멸자 호출");
+        }
     }
 
     class MyMath
@@ -228,6 +232,8 @@ namespace CSPractice1
             Console.WriteLine("두 번째 위치");
             Sample sample = new Sample();
             Console.WriteLine("세 번째 위치");
+
+            // #5 22-7 소멸자
 
 
         }
