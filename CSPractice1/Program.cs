1a28c8d19b5cb6b38d77e4b24c3c6094b645c583
munhyebin <munhyebin40@gmail.com>
2024-07-01 13:40:39 +0900

    #5 22-3 클래스 메서드

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -6,6 +6,19 @@ using System.Threading.Tasks;
 
 namespace CSPractice1
 { 
+    class MyMath
+    {
+        public static int classVar = 1;
+        public int instanceVar = 2;
+        public static int Abs(int input)
+        {
+            Console.WriteLine(classVar);
+            // 클래스 메서드에서 인스턴스 변수 접근 불가
+            // Console.WriteLine(instanceVar);
+            return (input >= 0) ? input : -input;
+        }
+    }
+
     class FirstClass
     {
 
@@ -182,6 +195,9 @@ namespace CSPractice1
             // #4 21-5 클래스 변수
             Console.WriteLine(Product.TAX_PATIO);
             //Console.WriteLine(p1.TAX_PATIO);  // Java는 가능하나 C#에서는 X
+
+            // #5 22-3 클래스 메서드
+
 
 
         }
